777ad48d2ead993c34c318326cdfd560dcf9890c
Brooks Becton <broabect@ut.utm.edu>
2018-03-21 15:28:12 -0500

    Added preinstall script and Procfile

--- src/resources/index.js
+++ src/resources/index.js
@@ -2,13 +2,11 @@ import { Router } from "express";
 import PostController from "./controllers/PostController";
 import SourceController from "./controllers/SourceController";
 import UserController from "./controllers/UserController";
-import UserPostRatingController from "./controllers/UserController/UserPostRatingController";
 
-export default function () {
+export default function() {
   var api = Router();
   api.use("/post", new PostController().route());
   api.use("/source", new SourceController().route());
   api.use("/user", new UserController().route());
-  api.use("/user/post/rating", new UserPostRatingController().route());
   return api;
 }
